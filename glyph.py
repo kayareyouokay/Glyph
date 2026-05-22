62cec8e54889a3c947a629d0824d5cb91d886f99
kayareyouokay <krishbhardwaj0619@gmail.com>
2026-05-22 23:33:47 +0530

    color output using ansi escape codes

--- glyph.py
+++ glyph.py
@@ -45,12 +45,19 @@ def to_ascii(brightness, invert=False):
     return ASCII_CHARS[idx]
 
 
-def render(pixels, method='average', invert=False):
+def render(pixels, method='average', invert=False, color=False):
     for row in pixels:
         line = ''
         for (r, g, b) in row:
             b_val = to_brightness(r, g, b, method)
-            line += to_ascii(b_val, invert) * 3
+            char = to_ascii(b_val, invert) * 3
+            if color:
+                # ansi truecolor escape: \033[38;2;R;G;Bm
+                line += f'\033[38;2;{r};{g};{b}m{char}'
+            else:
+                line += char
+        if color:
+            line += '\033[0m'
         print(line)
 
 
@@ -60,9 +67,10 @@ if __name__ == '__main__':
     parser.add_argument('--width', type=int, default=120)
     parser.add_argument('--method', choices=['average', 'minmax', 'luminosity'], default='average')
     parser.add_argument('--invert', action='store_true')
+    parser.add_argument('--color', action='store_true')
     args = parser.parse_args()
 
     img = load_image(args.image)
     img = resize_image(img, args.width)
     pixels = get_pixels(img)
-    render(pixels, args.method, args.invert)
+    render(pixels, args.method, args.invert, args.color)
